38638a80bb5a5a526589131b8458866fd919c1b2
ralfs marko <ralfssabovics@Opti-13.localdomain>
2026-02-26 11:28:37 +0200

    form created and start of controler

---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="/posts">
+        @csrf
+        <label for="title" >title</label><br>
+        <input type="text" name="titel"><br>
+        <label for="author" >author</label><br>
+        <input type="text" name="author"><br>
+        <label for="released_at" >released</label><br>
+        <input type="text" name="released_at"><br>
+        <label for="discription" >discriptione</label><br>
+        <input type="text" name="discription"><br>
+    </form>
+</body>
+</html>
